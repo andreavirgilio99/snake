b78a7b13079f0bfbe3ea36db90e41518daa378cd
andreavirgilio99 <andrea.virgilio@tsi-srl.it>
2023-09-14 17:17:42 +0200

    it fucking flickers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'snake';
+
+  x = 0; //snake x
+  y = 0; //snake y
+
+  mouseX = 0;
+  mouseY = 0;
+
+  speed = 20; // Velocità di movimento del serpente
+  interval: any; // Variabile per l'intervallo
+
+  @HostListener('document:mousemove', ['$event'])
+  mouseEvent(event: MouseEvent) {
+    this.mouseX = event.clientX;
+    this.mouseY = event.clientY;
+  }
+  
+    ngOnInit(): void {
+      this.interval = setInterval(() => {
+        // Calcola la differenza tra la posizione del serpente e quella del cursore
+        const dx = this.x - this.mouseX;
+        const dy = this.y - this.mouseY;
+  
+        // Muovi il serpente verso la posizione del cursore
+        this.x -= dx / this.speed;
+        this.y -= dy / this.speed;
+      }, 16); // Aggiorna la posizione ogni 16 ms (circa 60 FPS)
+    }
 }
