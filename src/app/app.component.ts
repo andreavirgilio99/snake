2b0b8c6b44a9932b196ea9d771e7bb51e19dbf30
andreavirgilio99 <andrea.virgilio@tsi-srl.it>
2023-09-15 17:08:01 +0200

    more realistic movements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 
-type Coordinates = {x: number, y: number}
+type Coordinates = { x: number; y: number };
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'snake';
@@ -13,89 +13,81 @@ export class AppComponent implements OnInit {
   mouseX = 0;
   mouseY = 0;
 
-  snake: Coordinates[] = [{x: 0, y: 0}]
+  snake: Coordinates[] = [{ x: 0, y: 0 }];
   food: Coordinates[] = [];
 
   speed = 50; // più è alta più è lento
-  movementIntervals: {interval: any, waitTime: number}[] = [];
+  movementInterval: any;
+  foodInterval: any;
 
   @HostListener('document:mousemove', ['$event'])
   mouseEvent(event: MouseEvent) {
     this.mouseX = event.clientX;
     this.mouseY = event.clientY;
   }
-  
-    ngOnInit(): void {
-      this.snakeMovementConfig();
-      this.foodSpawningConfig();
-    }
 
-    snakeMovementConfig(){
-      this.snake.forEach((segment, index) =>{
-        const waitTime = 16 + index;
-        const segInterval = setInterval(() => {
-          // Calcola la differenza tra la posizione del serpente e quella del cursore
-          const dx = segment.x - this.mouseX;
-          const dy = segment.y - this.mouseY;
-    
-          // Muovi il serpente verso la posizione del cursore
-          segment.x -= dx / this.speed;
-          segment.y -= dy / this.speed;
+  ngOnInit(): void {
+    this.snakeMovementConfig();
+    this.foodSpawningConfig();
+  }
 
-          this.checkFoodCollision(segment)
-        }, waitTime); // Aggiorna la posizione ogni 16 ms (circa 60 FPS) + index
+  snakeMovementConfig() {
+    this.movementInterval = setInterval(() => {
+      // Calcola la direzione dalla testa ai nuovi segmenti
+      for (let i = this.snake.length - 1; i >= 1; i--) {
+        const dx = this.snake[i - 1].x - this.snake[i].x;
+        const dy = this.snake[i - 1].y - this.snake[i].y;
 
-        this.movementIntervals.push({interval: segInterval, waitTime: waitTime})
-      })
-    }
+        this.snake[i].x += dx / this.speed;
+        this.snake[i].y += dy / this.speed;
 
-    getRandomPosition(): Coordinates{
-      const screenWidth = window.innerWidth;
-      const screenHeight = window.innerHeight;
-      const randomX = Math.random() * screenWidth;
-      const randomY = Math.random() * screenHeight;
+        this.checkFoodCollision(this.snake[i]);
+      }
 
-      return {x: randomX, y: randomY}
-    }
+      // Calcola la direzione dalla testa al mouse
+      const dx = this.mouseX - this.snake[0].x;
+      const dy = this.mouseY - this.snake[0].y;
 
-    foodSpawningConfig(){
-      setInterval(() =>{
-        this.food.push(this.getRandomPosition())
-      }, 5 * 1000)
-    }
+      this.snake[0].x += dx / this.speed;
+      this.snake[0].y += dy / this.speed;
 
-    checkFoodCollision(segment: Coordinates) {
-      for(let i = 0; i < this.food.length; i++) {
-        const food = this.food[i];
-        const distance = Math.sqrt(Math.pow(segment.x - food.x, 2) + Math.pow(segment.y - food.y, 2));
-        
-        if (distance < 10) {
-          this.food.splice(i, 1);
-          this.addSnakeSegment();
-          break;
-        }
+      this.checkFoodCollision(this.snake[0]);
+    }, 16);
+  }
+
+  getRandomPosition(): Coordinates {
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+    const randomX = Math.random() * screenWidth;
+    const randomY = Math.random() * screenHeight;
+
+    return { x: randomX, y: randomY };
+  }
+
+  foodSpawningConfig() {
+    this.foodInterval = setInterval(() => {
+      this.food.push(this.getRandomPosition());
+    }, 5 * 1000);
+  }
+
+  checkFoodCollision(segment: Coordinates) {
+    for (let i = 0; i < this.food.length; i++) {
+      const food = this.food[i];
+      const distance = Math.sqrt(
+        Math.pow(segment.x - food.x, 2) + Math.pow(segment.y - food.y, 2)
+      );
+
+      if (distance < 10) {
+        this.food.splice(i, 1);
+        this.addSnakeSegment();
+        break;
       }
     }
+  }
 
-    addSnakeSegment(){ //dovrebbe essere alle coordinate dell'ultimo segmento
-      const lastSegment = this.snake[this.snake.length -1];
-      const newSegment: Coordinates = {x: lastSegment.x, y: lastSegment.y}
-      this.snake.push(newSegment)
-
-      const lastInterval = this.movementIntervals.length -1;
-      const waitTime = 16 + ((this.movementIntervals[lastInterval].waitTime -16) +1);
-
-        const segInterval = setInterval(() => {
-          
-          const dx = newSegment.x - this.mouseX;
-          const dy = newSegment.y - this.mouseY;
-    
-          newSegment.x -= dx / this.speed;
-          newSegment.y -= dy / this.speed;
-
-          this.checkFoodCollision(newSegment)
-        }, waitTime);
-
-        this.movementIntervals.push({interval: segInterval, waitTime: waitTime})
-    }
+  addSnakeSegment() {
+    const lastSegment = this.snake[this.snake.length - 1];
+    const newSegment: Coordinates = { x: lastSegment.x, y: lastSegment.y };
+    this.snake.push(newSegment);
+  }
 }
